e1dfb364440eb4b5e8e225494e53cd018fb09ea8
Odonno <bottiau.david@laposte.net>
2019-01-01 17:03:59 +0100

    feat: use json-source-map to detect unique json node on each line of the file and create decorator

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
-import { ExtensionContext, workspace, TextDocument, window, Range, Position, TextLine } from 'vscode';
+import { ExtensionContext, workspace, TextDocument, window, Range, Position } from 'vscode';
 import { convertJsonIntoObject, extractInsights, convertSizeToString, Node } from './core';
+const jsonMap = require('json-source-map');
 
 /**
  * this method is called when your extension is activated
@@ -34,48 +35,90 @@ const processActiveFile = async (document: TextDocument) => {
 
 		const o = convertJsonIntoObject(text);
 		if (!!o) {
-			const documentLines = getAllDocumentLines(document);
 			const insights = extractInsights(o);
+			const jsonMapping = jsonMap.parse(text).pointers;
 
 			getEditors(document.fileName).forEach(editor => {
 				editor.setDecorations(
 					decorationType,
-					getDecorations(documentLines, insights)
+					getDecorations(insights, jsonMapping)
 				);
 			});
 		}
 	}
 }
 
-const getAllDocumentLines = (document: TextDocument) => {
-	return [...Array(document.lineCount).keys()].map(i => {
-		return document.lineAt(i);
-	});
-}
-
 const getEditors = (fileName: string) => {
 	return window.visibleTextEditors.filter(editor => editor.document.fileName === fileName);
 }
 
-const getDecorations = (documentLines: TextLine[], insights: Node) => {
-	return documentLines
-		.filter(line => !line.isEmptyOrWhitespace)
-		.map(({ lineNumber }, index) => {
-			// detect first line of the json content
-			if (index === 0) {
-				const firstLineDecorationOptions = {
-					renderOptions: {
-						after: {
-							contentText: convertSizeToString(insights.size) + " - " + insights.type,
-						}
-					},
-					range: new Range(new Position(lineNumber, 1024), new Position(lineNumber, 1024))
-				};
-				return [firstLineDecorationOptions];
+const getDecorations = (insights: Node, jsonMapping: any) => {
+	// detect first line of the json content
+	const firstLine = jsonMapping[''].value.line;
+	const firstLineDecorationOptions = {
+		renderOptions: {
+			after: {
+				contentText: convertSizeToString(insights.size) + " - " + insights.type,
 			}
+		},
+		range: new Range(new Position(firstLine, 1024), new Position(firstLine, 1024))
+	};
 
-			// TODO : detect & create decorations for children nodes
-			return [];
+	// detect & create decorations for children nodes
+	const jsonMapKeyValues = Object.keys(jsonMapping)
+		.map(key => {
+			return {
+				key,
+				content: jsonMapping[key]
+			};
 		})
-		.reduce((x, y) => x.concat(y));
+		.filter(x => !!x.content.key);
+
+	const linesWithJsonKey = jsonMapKeyValues
+		.map(x => x.content.key.line)
+		.filter(line => line > firstLine)
+		.filter((item, index, array) => array.indexOf(item) === index);
+
+	const linesDecorationOptions = linesWithJsonKey
+		.map(line => {
+			const mappedKeyValues = jsonMapKeyValues.filter(x => x.content.key.line === line);
+			return { 
+				line, 
+				node: findNodeByKey(insights, mappedKeyValues[0].key), 
+				numberOfNodes: mappedKeyValues.length 
+			};
+		})
+		.filter(x => !!x.node)
+		.filter(x => x.numberOfNodes === 1)
+		.map(({ line, node }) => {
+			return {
+				renderOptions: {
+					after: {
+						contentText: node ? (convertSizeToString(node.size) + " - " + node.type) : "",
+					}
+				},
+				range: new Range(new Position(line, 1024), new Position(line, 1024))
+			};
+		});
+
+	return [firstLineDecorationOptions].concat(linesDecorationOptions);
+}
+
+const findNodeByKey = (insights: Node, key: string) => {
+	const findNodeFromPath = (node: Node, path: string[]): Node | null => {
+		if (path.length === 0) {
+			return node;
+		}
+
+		if (node.value !== null && typeof node.value !== "boolean" && typeof node.value !== "number" && typeof node.value !== "string") {
+			return findNodeFromPath(
+				node.value.filter(n => n.property === path[0])[0],
+				path.filter((_, i) => i > 0)
+			);
+		}
+		return null;
+	}
+
+	const path = key.split('/').filter((_, i) => i > 0);
+	return findNodeFromPath(insights, path);
 }
